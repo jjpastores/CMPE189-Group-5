5e5021a81304d85ea720e2fc6f85a031301ca932
jjpastores <124344350+jjpastores@users.noreply.github.com>
2026-05-06 14:47:00 -0700

    added server change functionality

--- loadBalancer.py
+++ loadBalancer.py
@@ -7,35 +7,49 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet, arp, ipv4, tcp, udp, ether_types
 
 
+
+
 class DynamicLoadBalancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
+
     VIP = "10.0.0.10"
     VMAC = "00:00:00:00:00:10"
+
 
     SERVERS = {
         "10.0.0.5": {
             "mac": "00:00:00:00:00:05",
             "port": 5,
-            "connections": 0
+            "connections": 0,
+            "is_up": True
         },
         "10.0.0.6": {
             "mac": "00:00:00:00:00:06",
             "port": 6,
-            "connections": 0
+            "connections": 0,
+            "is_up": True
         }
     }
+
 
     def __init__(self, *args, **kwargs):
         super(DynamicLoadBalancer, self).__init__(*args, **kwargs)
         self.flow_to_server = {}
-        self.logger.info("Dynamic Least-Connections Load Balancer Started")
+        self.logger.info("Dynamic Least-Connections Load Balancer With Health Checks Started")
+
+
+        # Demo option:
+        # Uncomment this line to simulate server 10.0.0.5 being down.
+        self.mark_server_down("10.0.0.5")
+
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         parser = datapath.ofproto_parser
         ofp = datapath.ofproto
+
 
         match = parser.OFPMatch()
         actions = [
@@ -45,8 +59,10 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             )
         ]
 
+
         self.add_flow(datapath, 0, match, actions)
         self.logger.info("Switch connected")
+
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
@@ -56,25 +72,32 @@ class DynamicLoadBalancer(app_manager.RyuApp):
         ofp = datapath.ofproto
         in_port = msg.match["in_port"]
 
+
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
+
 
         if eth is None:
             return
 
+
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
 
+
         arp_pkt = pkt.get_protocol(arp.arp)
         ip_pkt = pkt.get_protocol(ipv4.ipv4)
+
 
         if arp_pkt:
             self.handle_arp(datapath, parser, ofp, in_port, eth, arp_pkt)
             return
 
+
         if ip_pkt:
             self.handle_ipv4(datapath, parser, ofp, in_port, eth, ip_pkt, pkt, msg.data)
             return
+
 
     def handle_arp(self, datapath, parser, ofp, in_port, eth, arp_pkt):
         if arp_pkt.opcode == arp.ARP_REQUEST and arp_pkt.dst_ip == self.VIP:
@@ -89,16 +112,20 @@ class DynamicLoadBalancer(app_manager.RyuApp):
                 src_ip=self.VIP
             )
 
+
     def handle_ipv4(self, datapath, parser, ofp, in_port, eth, ip_pkt, pkt, data):
         if ip_pkt.dst != self.VIP:
             return
+
 
         proto = ip_pkt.proto
         src_port = None
         dst_port = None
 
+
         tcp_pkt = pkt.get_protocol(tcp.tcp)
         udp_pkt = pkt.get_protocol(udp.udp)
+
 
         if tcp_pkt:
             src_port = tcp_pkt.src_port
@@ -109,8 +136,10 @@ class DynamicLoadBalancer(app_manager.RyuApp):
         else:
             return
 
+
         client_ip = ip_pkt.src
         client_mac = eth.src
+
 
         flow_key = (
             client_ip,
@@ -120,24 +149,49 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             dst_port
         )
 
+
         if flow_key in self.flow_to_server:
             server_ip = self.flow_to_server[flow_key]
+
+
+            if not self.SERVERS[server_ip]["is_up"]:
+                self.logger.warning(
+                    "Existing flow points to DOWN server %s. Dropping packet.",
+                    server_ip
+                )
+                return
+
+
         else:
             server_ip = self.choose_least_connection_server()
+
+
+            if server_ip is None:
+                self.logger.warning("Dropping packet: no healthy backend servers")
+                return
+
+
             self.flow_to_server[flow_key] = server_ip
             self.SERVERS[server_ip]["connections"] += 1
 
+
         server = self.SERVERS[server_ip]
 
+
         self.logger.info(
-            "Flow %s assigned to server %s | connections=%s",
+            "Flow %s assigned to server %s | connections=%s | health=%s",
             flow_key,
             server_ip,
             {
                 ip: self.SERVERS[ip]["connections"]
                 for ip in self.SERVERS
+            },
+            {
+                ip: self.SERVERS[ip]["is_up"]
+                for ip in self.SERVERS
             }
         )
+
 
         self.install_flows(
             datapath=datapath,
@@ -153,11 +207,13 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             dst_port=dst_port
         )
 
+
         actions = [
             parser.OFPActionSetField(ipv4_dst=server_ip),
             parser.OFPActionSetField(eth_dst=server["mac"]),
             parser.OFPActionOutput(server["port"])
         ]
+
 
         out = parser.OFPPacketOut(
             datapath=datapath,
@@ -167,13 +223,38 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             data=data
         )
 
+
         datapath.send_msg(out)
 
+
     def choose_least_connection_server(self):
+        healthy_servers = [
+            ip for ip in self.SERVERS
+            if self.SERVERS[ip]["is_up"]
+        ]
+
+
+        if not healthy_servers:
+            return None
+
+
         return min(
-            self.SERVERS,
+            healthy_servers,
             key=lambda ip: self.SERVERS[ip]["connections"]
         )
+
+
+    def mark_server_down(self, server_ip):
+        if server_ip in self.SERVERS:
+            self.SERVERS[server_ip]["is_up"] = False
+            self.logger.warning("Server %s marked DOWN", server_ip)
+
+
+    def mark_server_up(self, server_ip):
+        if server_ip in self.SERVERS:
+            self.SERVERS[server_ip]["is_up"] = True
+            self.logger.info("Server %s marked UP", server_ip)
+
 
     def install_flows(
         self,
@@ -199,6 +280,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
                 tcp_dst=dst_port
             )
 
+
             match_to_client = parser.OFPMatch(
                 eth_type=0x0800,
                 ip_proto=6,
@@ -207,6 +289,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
                 tcp_src=dst_port,
                 tcp_dst=src_port
             )
+
 
         elif proto == 17:
             match_to_server = parser.OFPMatch(
@@ -218,6 +301,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
                 udp_dst=dst_port
             )
 
+
             match_to_client = parser.OFPMatch(
                 eth_type=0x0800,
                 ip_proto=17,
@@ -227,8 +311,10 @@ class DynamicLoadBalancer(app_manager.RyuApp):
                 udp_dst=src_port
             )
 
+
         else:
             return
+
 
         actions_to_server = [
             parser.OFPActionSetField(ipv4_dst=server_ip),
@@ -236,12 +322,14 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             parser.OFPActionOutput(server["port"])
         ]
 
+
         actions_to_client = [
             parser.OFPActionSetField(ipv4_src=self.VIP),
             parser.OFPActionSetField(eth_src=self.VMAC),
             parser.OFPActionSetField(eth_dst=client_mac),
             parser.OFPActionOutput(client_port)
         ]
+
 
         self.add_flow(
             datapath,
@@ -253,6 +341,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             send_flow_rem=True
         )
 
+
         self.add_flow(
             datapath,
             priority=20,
@@ -263,21 +352,27 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             send_flow_rem=False
         )
 
+
     @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
     def flow_removed_handler(self, ev):
         msg = ev.msg
         match = msg.match
 
+
         if "ipv4_src" not in match or "ipv4_dst" not in match:
             return
+
 
         client_ip = match["ipv4_src"]
         vip = match["ipv4_dst"]
 
+
         if vip != self.VIP:
             return
 
+
         proto = match.get("ip_proto")
+
 
         if proto == 6:
             src_port = match.get("tcp_src")
@@ -288,6 +383,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
         else:
             return
 
+
         flow_key = (
             client_ip,
             self.VIP,
@@ -296,11 +392,14 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             dst_port
         )
 
+
         server_ip = self.flow_to_server.pop(flow_key, None)
+
 
         if server_ip and server_ip in self.SERVERS:
             if self.SERVERS[server_ip]["connections"] > 0:
                 self.SERVERS[server_ip]["connections"] -= 1
+
 
             self.logger.info(
                 "Flow expired: %s removed from %s | connections=%s",
@@ -311,6 +410,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
                     for ip in self.SERVERS
                 }
             )
+
 
     def send_arp_reply(
         self,
@@ -329,6 +429,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             ethertype=ether_types.ETH_TYPE_ARP
         )
 
+
         arp_reply = arp.arp(
             hwtype=1,
             proto=0x0800,
@@ -341,14 +442,17 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             dst_ip=dst_ip
         )
 
+
         pkt = packet.Packet()
         pkt.add_protocol(eth_reply)
         pkt.add_protocol(arp_reply)
         pkt.serialize()
 
+
         actions = [
             parser.OFPActionOutput(ofp.OFPP_IN_PORT)
         ]
+
 
         out = parser.OFPPacketOut(
             datapath=datapath,
@@ -358,7 +462,9 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             data=pkt.data
         )
 
+
         datapath.send_msg(out)
+
 
     def add_flow(
         self,
@@ -373,6 +479,7 @@ class DynamicLoadBalancer(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         ofp = datapath.ofproto
 
+
         inst = [
             parser.OFPInstructionActions(
                 ofp.OFPIT_APPLY_ACTIONS,
@@ -380,9 +487,11 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             )
         ]
 
+
         flags = 0
         if send_flow_rem:
             flags = ofp.OFPFF_SEND_FLOW_REM
+
 
         mod = parser.OFPFlowMod(
             datapath=datapath,
@@ -394,4 +503,8 @@ class DynamicLoadBalancer(app_manager.RyuApp):
             flags=flags
         )
 
+
         datapath.send_msg(mod)
+
+
+
